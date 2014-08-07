48678ea73bc7c0de22e7b5980c1305b991b652fe
maryhipp <maryhipp@gmail.com>
2014-08-07 17:11:34 -0400

    added hover description

---
+++ portfolio.js
@@ -0,0 +1,8 @@
+var $projectImage = $("#work .project img")
+var $projectDescription = $("#work .project .description")
+
+// $projectDescription.hide();
+
+// $projectImage.on('mouseover', function() {
+//   $projectDescription.fadeIn(500);
+// })
