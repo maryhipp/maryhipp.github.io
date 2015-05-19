ae45df9740063713706e64c79e6c55f7bbabbef9
maryhipp <maryhipp@gmail.com>
2015-05-18 21:47:39 -0400

    nav change and removals

---
+++ js/portfolio/portfolio.js
@@ -0,0 +1,15 @@
+$(window).scroll(function() {
+  var scroll = $(window).scrollTop();
+
+
+})
+
+// $(window).load(function() {
+//   $('#nav').on('mouseenter',function() {
+//     $('#nav').css('opacity', 1);
+//   })
+//   $('#nav').on('mouseleave',function() {
+//     $('#nav').css('opacity', 0.5);
+//   })
+
+// })
